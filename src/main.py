b0422ab0565e930acbf8757c2bc33685c7f5a369
kb29x37 <l.coulon25480@gmail.com>
2018-03-31 16:54:51 -0400

    train WIP

--- src/main.py
+++ src/main.py
@@ -25,7 +25,7 @@ index_list = np.arange(0, train_size)
 class D(nn.module):
     def __init__(self):
         super(D, self).__init__()
-        self.d_loss = nn.BCELoss()
+        self.loss = nn.BCELoss()
 
     def forward(self, x):
         pass
@@ -33,48 +33,55 @@ class D(nn.module):
 class G(nn.module):
     def __init__(self):
         super(G, self).__init__()
-        self.g_loss = nn.BCELoss()
+        self.loss = nn.BCELoss()
 
     def forward(self, x):
         pass
 
-
-def train(batch):
-    for i in range(0, epoch):
-        for k in range(0, D_steps):
-            D.zero_grad()
-
-            size, data_batch_temp = create_data_batch()
-            z = create_noise_batch()
-
-            noise_batch = G(Variable(z))
-            data_batch = Variable(data_batch_temp)
-            pass
+class Trainer():
+    def __init__(self):
+        self.D = D()
+        self.G = G()
+        self.D_optimiser = optim.Adam(D.parameters(), lr = lr)
+        self.G_optimiser = optim.Adam(G.parameters(), lr = lr)
 
 
-def create_noise_batch():
-    G_in = np.random.normal(0.0, 1.0, [minibatch_size, G_inputs])
-    return G_in
+    def train(self):
+        for i in range(0, epoch):
+            for k in range(0, D_steps):
+                D.zero_grad()
 
-def create_data_batch():
-    size = np.min(minibatch_size, index_list.shape[0])
+                size, data_batch = create_data_batch()
+                z = create_noise_batch()
+                noise_batch = G(Variable(z))
 
-    if(size == 0):
-        return 0, []
+                loss_d_r = self.D.loss(D(Variable(data_batch)))
+                loss_d_r.backward()
 
-    indices = np.random.randint(0, index_list.shape[0], size = size)
-    names = index_list[indices]
-    index_list.remove(names)
-    batch = np.zeros([size, image_x * image_y])
+                loss_d_f = self.D.loss(1 - D(noise_batch))
+                loss_d_f.backward()
+                self.D_optimiser.step()
+                pass
 
-    for i in range (0, size):
-        batch[i] = np.load(name[i] + ".npy")
+    def create_noise_batch(self):
+        G_in = np.random.normal(0.0, 1.0, [minibatch_size, G_inputs])
+        return G_in
 
-    return size, batch
+    def create_data_batch(self):
+        size = np.min(minibatch_size, index_list.shape[0])
 
-D = D()
-G = G()
+        if(size == 0):
+            return 0, []
 
-D_optimiser = optim.adam(D.parameters(), lr=lr)
+        indices = np.random.randint(0, index_list.shape[0], size = size)
+        names = index_list[indices]
+        index_list.remove(names)
+        batch = np.zeros([size, image_x * image_y])
 
-create_noise_batch()
+        for i in range (0, size):
+            batch[i] = np.load(name[i] + ".npy")
+
+        return size, batch
+
+T = Trainer()
+t.train()
