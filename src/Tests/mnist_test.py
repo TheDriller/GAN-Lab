0137ab012b53b42bf618d726fbbbaed0d66fc5d1
david <david.abraham.1336@gmail.com>
2018-04-02 13:47:47 -0400

    Revert "Small changes in mnist models."
    This reverts commit e904bc99920ac9173e9791ea6e97a7928b01745d.

--- src/Tests/mnist_test.py
+++ src/Tests/mnist_test.py
@@ -10,13 +10,11 @@ from torch.autograd import Variable
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
 
-cuda = torch.cuda.is_available()
-
 # hyperparameters
 D_steps = 3
 epoch = 10
 G_steps = 1
-G_inputs = 100 # ???
+G_inputs = 1000 # ???
 image_x = 28
 image_y = 28
 minibatch_size = 100
@@ -42,32 +40,24 @@ class D(nn.Module):
         x = x.view(-1, 28*28)
         out = F.relu(self.fc1(x))
         out = F.relu(self.fc2(out))
-        out = F.sigmoid(self.fc3(out))
+        out = F.log_softmax(self.fc3(out))
         return out
 
 class G(nn.Module):
     def __init__(self):
         super(G, self).__init__()
         self.loss = nn.BCELoss()
-        self.fc1 = nn.Linear(G_inputs, 256)
-        self.fc2 = nn.Linear(256, 512)
-        self.fc3 = nn.Linear(512, image_y * image_x)
+        self.fc1 = nn.Linear(G_inputs, image_y * image_x)
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.sigmoid(self.fc3(x))
-        return x
+        return F.relu(self.fc1(x))
 
 class Trainer():
     def __init__(self):
         self.D = D()
-        if cuda:
-            self.D.cuda()
+        self.D.cuda()
         self.G = G()
-        if cuda:
-            self.G.cuda()
-
+        self.G.cuda()
         self.D_optimiser = optim.Adam(self.D.parameters(), lr = lr)
         self.G_optimiser = optim.Adam(self.G.parameters(), lr = lr)
         self.predictions = []
@@ -79,44 +69,32 @@ class Trainer():
                 print("Epoch: " + str(i) + ", D step: " + str(k))
 #                for batch_id in range(int((train_size - 1) / minibatch_size) + 1):
                 for batch_idx, (x, target) in enumerate(train_loader):
-                    print(batch_idx)
                     self.D.zero_grad()
 
 #                    size, data_batch = self.create_data_batch()
                     z = Variable(self.create_noise_batch())
-                    if cuda:
-                        z = z.cuda()
+                    z = z.cuda()
                     generated_batch = self.G(z).detach()
                     x = Variable(x)
-                    if cuda:
-                        x = x.cuda()
+                    x = x.cuda()
                     real_prediction = self.D(x)[0] # 1x1
 
-                    y_ones = Variable(torch.ones(1))
-                    if cuda:
-                        y_ones = y_ones.cuda()
-                    loss_d_r = self.D.loss(real_prediction, y_ones)
+                    loss_d_r = self.D.loss(real_prediction, Variable(torch.ones(1)).cuda())
                     loss_d_r.backward()
 
                     generated_prediction = self.D(generated_batch)[0] # 1x1
-                    loss_d_f = self.D.loss(1 - generated_prediction, y_ones)
+                    loss_d_f = self.D.loss(1 - generated_prediction, Variable(torch.ones(1)).cuda())
                     loss_d_f.backward()
 
                     self.D_optimiser.step()
                 index_list = np.arange(0, train_size)
 
             for k in range(0, G_steps):
-                z = Variable(self.create_noise_batch())
-                if cuda:
-                    z = z.cuda()
-                generated_batch = self.G(z)
-
-                y_ones = Variable(torch.ones(1))
-                if cuda:
-                    y_ones = y_ones.cuda()
+                z = self.create_noise_batch()
+                generated_batch = self.G(Variable(z).cuda())
 
                 D_prediction = self.D(generated_batch)[0] # 1x1
-                loss_G = self.G.loss(D_prediction, y_ones)
+                loss_G = self.G.loss(D_prediction, Variable(torch.ones(1)).cuda())
                 loss_G.backward()
 
                 predictions.append(D_prediction.mean().data)
